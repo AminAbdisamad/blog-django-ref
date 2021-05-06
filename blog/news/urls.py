fbbc135bb8e0d81d54f86f070b0e8d63bda4bd01
Amin Abdisamad <amin.abdisamad@b4finance.com>
2021-05-06 05:24:37 +0300

    added contact

--- blog/news/urls.py
+++ blog/news/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('',views.home, name='news-home'),
-    path('about/',views.about, name='news-about')
+    path('about/',views.about, name='news-about'),
+    path('contact/',views.contact, name='news-contact')
 ]
