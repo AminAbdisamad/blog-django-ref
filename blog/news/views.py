45f04cad857350f5257deec94f5216c168792c2c
Amin Abdisamad <amin.abdisamad@b4finance.com>
2021-05-06 05:44:53 +0300

    added templates

--- blog/news/views.py
+++ blog/news/views.py
@@ -3,11 +3,14 @@ from django.http import HttpResponse
 
 # Create your views here.
 
+# def home(request):
+#     return HttpResponse('<h1> Hello there news app</h1>')
+
 def home(request):
-    return HttpResponse('<h1> Hello there news app</h1>')
+    return render(request,template_name="news/home.html")
 
 def about(request):
-    return HttpResponse('<h1>About news here</h1>')
+    return render(request,template_name="news/about.jinja")
 
 def contact(request):
     return HttpResponse('<h1>Contact here</h1>')
