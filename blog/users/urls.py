c3fd30cf7cd54ae283786c2c6925e8428986d03a
Amin Abdisamad <amin.abdisamad@b4finance.com>
2021-05-07 08:14:45 +0300

    user authentication added

--- blog/users/urls.py
+++ blog/users/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 
 
 urlpatterns = [
-    path('',views.auth, name='auth-login'),
+    # path('',views.auth, name='auth-login'),
+    path('',auth_views.LoginView.as_view(template_name="users/auth.jinja"), name='auth-login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name="users/logout.jinja"),name="auth-logout"),
     path('register/',views.register, name='auth-register'),
 ]
