bfb6d325d13ee758536662737d64b28eb4ecdca7
Amin Abdisamad <amin.abdisamad@b4finance.com>
2021-05-07 06:02:04 +0300

    added email and firtname for the userform

--- blog/users/views.py
+++ blog/users/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render,redirect
-from django.contrib.auth.forms import UserCreationForm
+from .forms import UserRegisterForm
 from django.contrib import messages
 
 # Create your views here.
@@ -9,14 +9,12 @@ def auth(request):
 
 def register(request):
     if request.method == 'POST':
-        print('I am Post request')
-        form = UserCreationForm(request.POST)
-      
+        form = UserRegisterForm(request.POST)
         if form.is_valid():
+            form.save()
             username = form.cleaned_data.get('username')
             messages.success(request, f'User with username {username} created successfully')
             return redirect('news-home')
     else:
-        print('Iam get request')
-        form = UserCreationForm()
+        form = UserRegisterForm()
     return render(request,'users/register.html',{'form':form})
