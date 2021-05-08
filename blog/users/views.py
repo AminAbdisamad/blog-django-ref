116cf7f38168173b6501d6a94bb883b5518e10f5
Amin Abdisamad <amin.abdisamad@b4finance.com>
2021-05-08 06:14:25 +0300

    user profile works

--- blog/users/views.py
+++ blog/users/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render,redirect
 from .forms import UserRegisterForm
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-
+from .forms import ProfileImageUpdate,UserUpdateForm
 
 
 def register(request):
@@ -19,4 +19,20 @@ def register(request):
 
 @login_required
 def profile(request):
-    return render(request,'users/profile.jinja')
+    if request.method == 'POST':
+        user_form = UserUpdateForm(request.POST,instance=request.user)
+        profile_form = ProfileImageUpdate(request.POST,request.FILES,instance=request.user.profile)
+        if user_form.is_valid() and profile_form.is_valid():
+            profile_form.save()
+            user_form.save()
+            messages.success(request, f'User information updated successfully')
+            return redirect('news-home')
+    else:
+        user_form = UserUpdateForm(instance=request.user)
+        profile_form = ProfileImageUpdate(instance=request.user.profile)
+    context = {
+        'user_form':user_form,
+        'profile_form':profile_form
+    }
+
+    return render(request,'users/profile.jinja',context)
