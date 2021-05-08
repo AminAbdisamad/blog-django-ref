de8ed64667c6fc9af41c123fb430d2b2833e53dc
Amin Abdisamad <amin.abdisamad@b4finance.com>
2021-05-09 02:45:41 +0300

    added Class Listview

--- blog/news/urls.py
+++ blog/news/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from . import views
+from .views import PostListView
 
 
 urlpatterns = [
-    path('',views.home, name='news-home'),
+    # path('',views.home, name='news-home'),
+    path('',views.PostListView.as_view(), name='news-home'),
     path('about/',views.about, name='news-about'),
     path('contact/',views.contact, name='news-contact')
 ]
--- blog/news/views.py
+++ blog/news/views.py
@@ -1,17 +1,29 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import ListView
 from .models import Post
 # Create your views here.
 
 # def home(request):
 #     return HttpResponse('<h1> Hello there news app</h1>')
 
+class PostListView(ListView):
+    # Where to get list of view
+    model = Post
+    # if not set context_object variable is available in template
+    context_object_name = 'posts'
+    # by default it looks for <app>/<model_list>.<extention> ex. news/post_list.html
+    template_name ="news/home.jinja"
+    ordering = '-date_posted'
 
-def home(request):
-    context = {
-        'posts':Post.objects.all()
-    }
-    return render(request,"news/home.jinja",context)
+
+
+
+# def home(request): 
+#     context = {
+#         'posts':Post.objects.all()
+#     }
+#     return render(request,"news/home.jinja",context)
 
 def about(request):
     return render(request,template_name="news/about.jinja")
