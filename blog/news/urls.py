97aa0c5a977ff43488a16ef6253b033166cac278
Amin Abdisamad <amin.abdisamad@b4finance.com>
2021-05-09 03:11:40 +0300

    added post detail view

--- blog/news/urls.py
+++ blog/news/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from . import views
-from .views import PostListView
+from .views import PostListView,PostDetailView
 
 
 urlpatterns = [
     # path('',views.home, name='news-home'),
     path('',views.PostListView.as_view(), name='news-home'),
+    path('post/<int:pk>/',views.PostDetailView.as_view(), name='post-detail'),
     path('about/',views.about, name='news-about'),
     path('contact/',views.contact, name='news-contact')
 ]
