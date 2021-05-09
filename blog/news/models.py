e586f1562ffcb25755b7228ea5cd6862d05f50c9
Amin Abdisamad <amin.abdisamad@b4finance.com>
2021-05-09 06:01:12 +0300

    Added post add and more

--- blog/news/models.py
+++ blog/news/models.py
@@ -1,16 +1,28 @@
 
 from django.db import models
+from django.urls import reverse
 from django.contrib.auth.models import User
 from django.utils import timezone
+from django.template.defaultfilters import slugify
 # Create your models here.
 
 
 class Post(models.Model):
     title = models.CharField(max_length=128)
+    slug = models.SlugField()
     content = models.TextField()
     date_posted = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
 
+    def save(self, *args, **kwargs):
+        if not self.pk:
+            self.slug = slugify(self.title)
+        super(Post, self).save(*args, **kwargs)
+
+    # Redirect to post details page
+    def get_absolute_url(self):
+        return reverse("post-detail", kwargs={"pk": self.pk})
+    
     def __repr__(self):
         return f'(Post) {self.title}'
         
--- blog/news/urls.py
+++ blog/news/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 from . import views
-from .views import PostListView,PostDetailView
+from .views import PostListView,PostDetailView,CreatePostView
 
 
 urlpatterns = [
     # path('',views.home, name='news-home'),
     path('',views.PostListView.as_view(), name='news-home'),
     path('post/<int:pk>/',views.PostDetailView.as_view(), name='post-detail'),
+    path('post/add/',views.CreatePostView.as_view(), name='post-create'),
     path('about/',views.about, name='news-about'),
     path('contact/',views.contact, name='news-contact')
 ]
